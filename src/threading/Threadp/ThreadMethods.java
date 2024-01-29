5383495b94fae4ad10255f787ba850def2c8a12d
Pradeepsoni-mph <pk.study40@gmail.com>
2024-01-29 22:42:15 +0530

    uncommented classes and methods and checked duplicacy

--- src/threading/Threadp/ThreadMethods.java
+++ src/threading/Threadp/ThreadMethods.java
@@ -1,60 +1,60 @@
 package threading.Threadp;
 
-//class A extends Thread
-//{
-//	public void run()
-//	{
-//		for(int i=1; i<=5; i++)
-//		{
-//			if(i==1) yield();
-//			System.out.println("\t Form Thread A: i=" + i);
-//		}
-//		System.out.println("Exit Form A");
-//	}
-//}
-//
-//class B extends Thread
-//{
-//	public void run()
-//	{
-//		for(int j=1; j<=5; j++)
-//		{
-//			System.out.println("\t Form Thread B: j=" + j);
-//			if(j==3) stop();
-//
-//		}
-//		System.out.println("Exit Form B");
-//	}
-//}
-//
-//class C extends Thread
-//{
-//	public void run()
-//	{
-//		for(int k=1; k<=5; k++)
-//		{
-//			System.out.println("\t Form Thread C: k=" + k);
-//			if(k==1)
-//			try
-//			{
-//				sleep(1000);
-//			}
-//			catch(Exception e)
-//			{
-//			}
-//		}
-//
-//		System.out.println("Exit Form A");
-//	}
-//}
+class A1 extends Thread
+{
+	public void run()
+	{
+		for(int i=1; i<=5; i++)
+		{
+			if(i==1){ Thread.currentThread().yield();}
+			System.out.println("\t Form Thread A: i=" + i);
+		}
+		System.out.println("Exit Form A");
+	}
+}
+
+class B1 extends Thread
+{
+	public void run()
+	{
+		for(int j=1; j<=5; j++)
+		{
+			System.out.println("\t Form Thread B: j=" + j);
+			if(j==3) stop();
+
+		}
+		System.out.println("Exit Form B");
+	}
+}
+
+class C1 extends Thread
+{
+	public void run()
+	{
+		for(int k=1; k<=5; k++)
+		{
+			System.out.println("\t Form Thread C: k=" + k);
+			if(k==1)
+			try
+			{
+				sleep(1000);
+			}
+			catch(Exception e)
+			{
+			}
+		}
+
+		System.out.println("Exit Form A");
+	}
+}
 
 class ThreadMethods
 {
 	public static void main(String arg[])
 	{
-		A threadA = new A();
-		B threadB = new B();
-		C threadC = new C();
+		A1 threadA = new A1();
+		B1 threadB = new B1();
+		C1 threadC = new C1();
 
 		System.out.println("Start thread A");
 		threadA.start();
